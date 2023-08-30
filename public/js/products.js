0fe00adfa6773bc9c4c6a70fcbc2b8ef53ed11f8
Aaron Hirvi <aaron.hirvi@tuni.fi>
2023-08-30 21:23:30 +0300

    add viewing of individual products

--- public/js/products.js
+++ public/js/products.js
@@ -1,4 +1,5 @@
 const productListContainer = document.getElementById('productList')
+const productInfoContainer = document.getElementById('productInfo')
 
 async function fetchProducts() {
   try {
@@ -10,17 +11,66 @@ async function fetchProducts() {
   }
 }
 
+async function fetchProduct() {
+  try {
+    const productId = window.history.state
+    const response = await fetch(
+      `http://127.0.0.1:3000/api/products/${productId}`
+    )
+    const product = await response.json()
+    return product
+  } catch (error) {
+    return {}
+  }
+}
+
+async function displaySingleProduct() {
+  const product = await fetchProduct()
+
+  productInfoContainer.innerHTML = ''
+  productListContainer.innerHTML = ''
+  const productElement = document.createElement('div')
+  productElement.classList.add('product')
+  productElement.textContent = `Name: ${product.name}, Price: ${product.price} ${product.description}`
+  productInfoContainer.appendChild(productElement)
+}
+
 async function displayProducts() {
   const products = await fetchProducts()
 
   productListContainer.innerHTML = ''
+  productInfoContainer.innerHTML = ''
 
   products.forEach((product) => {
     const productElement = document.createElement('div')
     productElement.classList.add('product')
+    productElement.onclick = (event) => {
+      event.preventDefault()
+      window.history.pushState(product.id, '', `/products/${product.id}`)
+      displaySingleProduct()
+    }
     productElement.textContent = `Name: ${product.name}, Price: ${product.price}`
     productListContainer.appendChild(productElement)
   })
 }
 
-window.addEventListener('load', displayProducts)
+window.addEventListener('popstate', (event) => {
+  if (event.state) {
+    displaySingleProduct()
+  } else {
+    displayProducts()
+  }
+})
+
+function render() {
+  if (window.history.state) {
+    displaySingleProduct()
+  } else {
+    displayProducts()
+  }
+}
+
+window.addEventListener('load', () => {
+  window.history.replaceState(null, '', '')
+  render()
+})
