5a4ee4d12566c44e0b9926546062c66b09b8b97f
Aaron Hirvi <aaron.hirvi@tuni.fi>
2023-09-01 14:35:53 +0300

    add product editing for admins

--- public/js/products.js
+++ public/js/products.js
@@ -39,17 +39,85 @@ async function displayProducts() {
   productListContainer.innerHTML = ''
   productInfoContainer.innerHTML = ''
 
-  products.forEach((product) => {
-    const productElement = document.createElement('div')
-    productElement.classList.add('product')
-    productElement.onclick = (event) => {
-      event.preventDefault()
-      window.history.pushState(product.id, '', `/products/${product.id}`)
-      displaySingleProduct()
+  products.forEach(async (product) => {
+    const productElement = createProductElement(product)
+
+    if (await isUserAdmin()) {
+      const editContainer = createEditContainer(product)
+      const showEditContainerButton = createShowEditButton(editContainer)
+      productElement.appendChild(editContainer)
+      productElement.appendChild(showEditContainerButton)
     }
-    productElement.textContent = `Name: ${product.name}, Price: ${product.price}`
     productListContainer.appendChild(productElement)
   })
+}
+
+function createProductElement(product) {
+  const productElement = document.createElement('div')
+  productElement.classList.add('product')
+  productElement.innerHTML = `<p>Name: ${product.name}, Price: ${product.price}</p>`
+
+  return productElement
+}
+
+function createEditContainer(product) {
+  const editContainer = document.createElement('div')
+  editContainer.style.display = 'none'
+
+  const editNameField = createInput('text', product.name)
+  const editPriceField = createInput('text', product.price)
+  const editDescriptionField = createInput('text', product.description)
+
+  const editButton = createEditButton(
+    product,
+    editNameField,
+    editPriceField,
+    editDescriptionField
+  )
+
+  editContainer.appendChild(editNameField)
+  editContainer.appendChild(editPriceField)
+  editContainer.appendChild(editDescriptionField)
+  editContainer.appendChild(editButton)
+
+  return editContainer
+}
+
+function createInput(type, value) {
+  const input = document.createElement('input')
+  input.type = type
+  input.value = value
+  return input
+}
+
+function createEditButton(product, nameField, priceField, descriptionField) {
+  const editButton = document.createElement('button')
+  editButton.textContent = 'Save'
+
+  editButton.onclick = async () => {
+    await fetchUrl(`/products/${product.id}`, 'PUT', {
+      name: nameField.value,
+      price: priceField.value,
+      description: descriptionField.value,
+    })
+    displayProducts()
+  }
+
+  return editButton
+}
+
+function createShowEditButton(editContainer) {
+  const showEditContainerButton = document.createElement('button')
+  showEditContainerButton.textContent = 'Edit'
+
+  showEditContainerButton.onclick = () => {
+    editContainer.style.display =
+      editContainer.style.display === 'none' ? 'block' : 'none'
+    showEditContainerButton.textContent =
+      showEditContainerButton.textContent === 'Edit' ? 'Hide' : 'Edit'
+  }
+
+  return showEditContainerButton
 }
 
 window.addEventListener('popstate', (event) => {
