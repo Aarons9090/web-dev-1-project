471f9564728ccdec45c8838571e40720cc82526b
Aaron Hirvi <aaron.hirvi@tuni.fi>
2023-09-05 10:23:12 +0300

    fix fake data

--- db-init.js
+++ db-init.js
@@ -3,6 +3,11 @@ const Product = require('./models/Product')
 const Role = require('./models/role')
 const User = require('./models/user')
 const { default: mongoose } = require('mongoose')
+const bcrypt = require('bcrypt')
+const config = require('./utils/config')
+const ProductService = require('./services/ProductService')
+
+const productService = new ProductService()
 
 const mockProducts = [
   {
@@ -60,18 +65,20 @@ async function loadData() {
       const role = await Role.findOne({
         name: user.username === 'admin' ? 'Admin' : 'Customer',
       })
+
+      const passwordHash = await bcrypt.hash(user.password, config.SALT_ROUNDS)
+
       const newUser = new User({
         username: user.username,
-        password: user.password,
+        password: passwordHash,
         role: role.id,
       })
       await newUser.save()
     })
 
-    // Wait for all product and user creation promises to resolve
+    // Wait for all product and user creation promises
     await Promise.all([...productPromises, ...userPromises])
 
-    // Disconnect from the database
     await mongoose.disconnect()
   } catch (error) {
     console.error('Error loading data:', error)
